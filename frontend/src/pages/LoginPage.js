144deab3028938bb20356f8a13c60bf424ce637e
Martin Mareš <martin.mares666@gmail.com>
2016-11-14 21:45:38 +0100

    test

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -37,39 +37,39 @@ class LoginPageRaw extends Component {
 
         <div className="row main">
           <div className="main-login main-center">
-            <h2 className="title">Logisn</h2>
+            <h2 className="title">Login</h2>
             <form className="form-horizontal" method="post" action="#">
 
               <div className="form-group">
                 <label htmlFor="email" className="cols-sm-2 control-label">Email</label>
                 <div className="cols-sm-10">
                   <div className="input-group">
-         <span className="input-group-addon"><i className="fa fa-envelope fa" aria-hidden="true"></i></span>
-         <input type="text" className="form-control" name="email" id="email"  placeholder="Enter your Email"/>
-        </div>
-       </div>
-      </div>
+                    <span className="input-group-addon"><i className="fa fa-envelope fa" aria-hidden="true"></i></span>
+                    <input type="text" className="form-control" name="email" id="email"  placeholder="Enter your Email"/>
+                  </div>
+                </div>
+              </div>
 
-      <div className="form-group">
-       <label htmlFor="password" className="cols-sm-2 control-label">Password</label>
-       <div className="cols-sm-10">
-        <div className="input-group">
-         <span className="input-group-addon"><i className="fa fa-lock fa-lg" aria-hidden="true"></i></span>
-         <input type="password" className="form-control" name="password" id="password"  placeholder="Enter your Password"/>
-        </div>
-       </div>
-      </div>
+              <div className="form-group">
+                <label htmlFor="password" className="cols-sm-2 control-label">Password</label>
+                <div className="cols-sm-10">
+                  <div className="input-group">
+                    <span className="input-group-addon"><i className="fa fa-lock fa-lg" aria-hidden="true"></i></span>
+                    <input type="password" className="form-control" name="password" id="password"  placeholder="Enter your Password"/>
+                  </div>
+                </div>
+              </div>
 
-      <div className="form-group ">
-       <button type="button" className="btn btn-primary btn-lg btn-block login-button">Login</button>
-      </div>
-      <div className="login-register">
+              <div className="form-group ">
+                <button type="button" className="btn btn-primary btn-lg btn-block login-button">Login</button>
+              </div>
+              <div className="login-register">
                 Not registered yet?  <Link to="/register">Register</Link>
-        </div>
-     </form>
-    </div>
+              </div>
+            </form>
+          </div>
 
-      </div>
+        </div>
       </div>
     );
   }
