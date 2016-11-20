50d0f126528691d19ec8ebfda9d6337e568fa096
Anatolii Zhiliaev <zhiliaev.anatolii@gmail.com>
2016-11-20 18:13:17 +0100

    Revert "Add Custom work model"
    This reverts commit 556fcfd39b327ecd107352b4dce399cb14c2b8bc.

--- backend/server/boot/create-sample-products.js
+++ backend/server/boot/create-sample-products.js
@@ -1,19 +1,20 @@
 'use strict';
 
 module.exports = function(app) {
-  app.dataSources.mysqlds.autoupdate('testModel', function(err) {
-    const { Testmodel } = app.models;
-    if (!Testmodel) { return; }
+  app.dataSources.mysqlds.autoupdate('EshopCategory', function(err) {
+    const { EshopCategory } = app.models;
+    if (!EshopCategory) { return; }
 
-    Testmodel.count({}, function(err, count) {
+    EshopCategory.count({}, function(err, count) {
       if (count !== 0) { return };
 
-      Testmodel.create([
+      EshopCategory.create([
         {
-          id: 1,
-          name: 'test',
+          name: 'Car',
         },
-
+        {
+          name: 'Motorbike',
+        }
       ], function(err, categories) {
         if (err) throw err;
 
@@ -21,29 +22,6 @@ module.exports = function(app) {
       });
     });
   });
-
-
-    app.dataSources.mysqlds.autoupdate('EshopCategory', function(err) {
-      const { EshopCategory } = app.models;
-      if (!EshopCategory) { return; }
-
-      EshopCategory.count({}, function(err, count) {
-        if (count !== 0) { return };
-
-        EshopCategory.create([
-          {
-            name: 'Car',
-          },
-          {
-            name: 'Motorbike',
-          }
-        ], function(err, categories) {
-          if (err) throw err;
-
-          console.log('Models created: \n', categories);
-        });
-      });
-    });
 
   app.dataSources.mysqlds.autoupdate('EshopProduct', function(err) {
     const { EshopProduct } = app.models;
@@ -78,83 +56,6 @@ module.exports = function(app) {
       });
     });
   });
-
-
-  app.dataSources.mysqlds.autoupdate('Customer', function(err) {
-    const { Customer } = app.models;
-    if (!Customer) { return; }
-
-    Customer.count({}, function(err, count) {
-      if (count !== 0) { return };
-
-      Customer.create([
-        {
-          email: 'test@mail.com',
-          password: 'testtest'
-        }
-      ], function(err, categories) {
-        if (err) throw err;
-
-        console.log('Models created: \n', categories);
-      });
-    });
-  });
-
-
-
-app.dataSources.mysqlds.autoupdate('Activity', function(err) {
-
-  const { Activity } = app.models;
-  if (!Activity) { return; }
-
-  Activity.count({}, function(err, count) {
-    if (count !== 0) { return };
-
-    Activity.create([
-      {
-        user_id: 10,
-        name: "test action name",
-        city: "prague",
-        address: "konevova 3",
-        date_and_time: new Date(),
-        user_count: 10,
-        about: "it's the best activity in prague"
-      }
-    ], function(err, categories) {
-      if (err) throw err;
-
-      console.log('Models created: \n', categories);
-    });
-  });
-
- });
-app.dataSources.mysqlds.autoupdate('Role', function(err) { });
-app.dataSources.mysqlds.autoupdate('ACL', function(err) { });
-app.dataSources.mysqlds.autoupdate('RoleMapping', function(err) { });
-app.dataSources.mysqlds.autoupdate('AccessToken', function(err) { });
-
-app.dataSources.mysqlds.autoupdate('User', function(err) {
-  const { User } = app.models;
-  if (!User) { return; }
-
-  User.count({}, function(err, count) {
-    if (count !== 0) { return };
-
-    User.create([
-      {
-        email: 'test@mail.com',
-        password: 'testtest'
-      }
-    ], function(err, categories) {
-      if (err) throw err;
-
-      console.log('Models created: \n', categories);
-    });
-  });
-});
-
-
-
   app.dataSources.mysqlds.autoupdate('EshopOrder', function(err) { });
   app.dataSources.mysqlds.autoupdate('EshopOrderItem', function(err) { });
 };
