f21243d1144ddcf6855eef02d84089d556abc70e
Daniel Sram <sramdaniel@gmail.com>
2016-12-11 18:01:49 +0100

    filtrovani vyhledanych uzivatelu

--- frontend/src/components/UserSearching/UserSearching.js
+++ frontend/src/components/UserSearching/UserSearching.js
@@ -1,42 +1,74 @@
 import React, { Component } from 'react';
-
 import SearchBar from 'search-bar-component';
 import { PageHeader, Grid} from 'react-bootstrap'
-import '../ActivityGrid/SearchBar.css';
+import { createFilter } from 'react-search-input'
+import lodash from 'lodash'
 
+import '../ActivityGrid/SearchBar.css';
 import { User } from './User.js'
+import api from '../../api.js';
 
 export class UserSearching extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      users: [],
+      searchTerm: null
+    }
+  }
 
+  componentDidMount() {
+    api('Customers').then((response) => {
+      this.setState({
+          users: response.data
+      })
+    })
   }
 
   _onSearchUser(username) {
-
+    this.setState({
+      searchTerm: username
+    })
   }
 
-  _header(){
+  _renderSearchBar(){
     return (
       <div>
         <PageHeader><h4>Najdi parťáka a pozvi ho na akci!</h4></PageHeader>
-        <SearchBar placeholder="Najdi parťáka..."
-          onChange={(username, resolve) => {this._onSearchUser(username)}}
+        <SearchBar
+          placeholder="Najdi parťáka..."
+          onChange={(username) => {this._onSearchUser(username)}}
           onSearch={(username) => {this._onSearchUser(username)}}
+          autoFocus
         />
         <br/>
       </div>
     )
   }
 
+  _getUsers(){
+    var { users, searchTerm } = this.state
+    if(searchTerm){
+      let usersByUsername = users.filter(createFilter(this.state.searchTerm, 'username'))
+      let usersByEmail = users.filter(createFilter(this.state.searchTerm, 'email'))
+      users = lodash.uniq(usersByUsername.concat(usersByEmail))
+    }
+    return users
+  }
+
   render() {
     return (
       <div className="container-fluid">
-        {this._header()}
+        {this._renderSearchBar()}
         <Grid>
-          <User name="Ekzim Hnědko" id={1} />
-          <User name="Daniel Šrám" id={2} />
+          {
+            this._getUsers().map( user => {
+              return (
+                <User name={user.username} id={user.id} />
+              )
+            })
+          }
         </Grid>
         <br/>
       </div>
