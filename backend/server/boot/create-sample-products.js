cb9323bf4cc88d178fed4d3f5c622374919676fd
Anatolii Zhiliaev <zhiliaev.anatolii@gmail.com>
2017-01-07 19:47:20 +0100

    Add new models Interest and HasInterest

--- backend/server/boot/create-sample-products.js
+++ backend/server/boot/create-sample-products.js
@@ -24,7 +24,7 @@ debugger;
       });
     });
   });
-
+/*
   app.dataSources.mysqlds.autoupdate('EshopProduct', function(err) {
     const { EshopProduct } = app.models;
     if (!EshopProduct) { return; }
@@ -58,7 +58,7 @@ debugger;
       });
     });
   });
-
+*/
 
   app.dataSources.mysqlds.autoupdate('Customer', function(err) {
     const { Customer } = app.models;
@@ -204,7 +204,98 @@ app.dataSources.mysqlds.autoupdate('User', function(err) {
 });
 
 
+
+app.dataSources.mysqlds.autoupdate('HasInterest', function(err) {
+    const { HasInterest } = app.models;
+    if (!HasInterest) { return; }
+
+    HasInterest.count({}, function(err, count) {
+      if (count !== 0) { return };
+
+      HasInterest.create([
+        { "interest_id": 1, customer_id: 2 }
+      ], function(err, HasInterest) {
+        if (err) throw err;
+
+        console.log('Models created: \n', HasInterest);
+      });
+    });
+  });
+
+
+app.dataSources.mysqlds.autoupdate('Interest', function(err) {
+    const { Interest } = app.models;
+    if (!Interest) { return; }
+
+    Interest.count({}, function(err, count) {
+      if (count !== 0) { return };
+
+      Interest.create([
+        { "interest": "Reading" },
+		{ "interest": "Watching TV" },
+		{ "interest": "Family Time" },
+		{ "interest": "Going to Movies" },
+		{ "interest": "Fishing" },
+		{ "interest": "Computer" },
+		{ "interest": "Gardening" },
+		{ "interest": "Renting Movies" },
+		{ "interest": "Walking" },
+		{ "interest": "Exercise" },
+		{ "interest": "Listening to Music" },
+		{ "interest": "Entertaining" },
+		{ "interest": "Hunting" },
+		{ "interest": "Team Sports" },
+		{ "interest": "Shopping" },
+		{ "interest": "Traveling" },
+		{ "interest": "Sleeping" },
+		{ "interest": "Socializing" },
+		{ "interest": "Sewing" },
+		{ "interest": "Golf" },
+		{ "interest": "Church Activities" },
+		{ "interest": "Relaxing" },
+		{ "interest": "Playing Music" },
+		{ "interest": "Housework" },
+		{ "interest": "Crafts" },
+		{ "interest": "Watching Sports" },
+		{ "interest": "Bicycling" },
+		{ "interest": "Playing Cards" },
+		{ "interest": "Hiking" },
+		{ "interest": "Cooking" },
+		{ "interest": "Eating Out" },
+		{ "interest": "Dating Online" },
+		{ "interest": "Swimming" },
+		{ "interest": "Camping" },
+		{ "interest": "Skiing" },
+		{ "interest": "Working on Cars" },
+		{ "interest": "Writing" },
+		{ "interest": "Boating" },
+		{ "interest": "Motorcycling" },
+		{ "interest": "Animal Care" },
+		{ "interest": "Bowling" },
+		{ "interest": "Painting" },
+		{ "interest": "Running" },
+		{ "interest": "Dancing" },
+		{ "interest": "Horseback Riding" },
+		{ "interest": "Tennis" },
+		{ "interest": "Theater" },
+		{ "interest": "Billiards" },
+		{ "interest": "Beach" },
+		{ "interest": "Volunteer Work" }
+		
+      ], function(err, Interest) {
+        if (err) throw err;
+
+        console.log('Models created: \n', Interest);
+      });
+    });
+  });
+
+
+
+
+ 
   app.dataSources.mysqlds.autoupdate('Profile', function(err) { });
   app.dataSources.mysqlds.autoupdate('EshopOrder', function(err) { });
   app.dataSources.mysqlds.autoupdate('EshopOrderItem', function(err) { });
+
 };
