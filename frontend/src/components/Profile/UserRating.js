d846ae711e32ace9c75a0a4bdd5cf8334395c518
Daniel Sram <sramdaniel@gmail.com>
2017-01-15 13:40:40 +0100

    merge

--- frontend/src/components/Profile/UserRating.js
+++ frontend/src/components/Profile/UserRating.js
@@ -1,24 +1,40 @@
 import React, { Component } from 'react';
 import Rating from 'react-rating-system';
+import { Well } from 'react-bootstrap';
 
+import { loadState } from '../../store/localState.js'
 import api from '../../api.js';
 
 const STAR_ICON = require('../../img/star.png')
+
+const Comment = (props) => (
+  <Well>
+    <p><em>{props.text}</em></p>
+    <p className="text-right"><em>- {props.author}</em></p>
+  </Well>
+);
 
 export class UserRating extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      rating: 0,
-      editable: true
-    }
+    this.userAlreadyVoted = false
+    this.loggedUserId = loadState().auth.userId;
     this._onRateUser = this._onRateUser.bind(this)
     this._loadRating = this._loadRating.bind(this)
+    this._preventRepeatedVote = this._preventRepeatedVote.bind(this)
+    this.state = {
+      rating: 0,
+      editable: true // this.props.customerId !== this.loggedUserId // prevent self-vote
+    }
+    this._preventRepeatedVote()
   }
 
   componentDidMount(){
     this._loadRating()
+    if(this.state.editable) {
+      // TODO this._preventRepeatedVote()
+    }
   }
 
   _loadRating(){
@@ -30,7 +46,18 @@ export class UserRating extends Component {
     })
   }
 
+  _preventRepeatedVote(){
+    let reqData = {
+      id: this.props.customerId
+    }
+    api('/Ratings/'+this.loggedUserId+'/status', reqData).then((response) => {
+      this.userAlreadyVoted = (typeof response.data !== 'boolean')
+    })
+  }
+
   _onRateUser(numberOfStars) {
+    console.log(`alreadyVoted`,this.userAlreadyVoted);
+    if(this.userAlreadyVoted){ return null }
     let reqData = {
       sympathy: numberOfStars,
       user_id: this.props.customerId
@@ -49,19 +76,25 @@ export class UserRating extends Component {
     return (
       <div>
         <h3>Mé hodnocení</h3>
-        <Rating
-          callback={this._onRateUser}
-          initialValue={rating}
-          editable={editable}
-          lockRating={true}
-          image={STAR_ICON}
-          fillBG="#f1c40f"
-          initialBG="#6a6a6a"
-          numberStars={5}
-          containerStyle={{ maxWidth: '200px' }}
-        />
-        <br/><br/><br/>
+        <div style={{display: 'flex', justifyContent: 'center'}}>
+          <Rating
+            callback={this._onRateUser}
+            initialValue={rating}
+            editable={editable}
+            lockRating={true}
+            image={STAR_ICON}
+            fillBG="#f1c40f"
+            initialBG="#6a6a6a"
+            numberStars={5}
+            containerStyle={{ maxWidth: '200px' }}
+          />
+        </div>
+        <br/>
         <h3>Napsali o mně</h3>
+        <Comment
+          text="Zatím Vás slovně nikdo neohodnotil!"
+          author="HobbyHub"
+        />
       </div>
     )
   }
